2ec626d745f12236294003b433571d3c938c2d5c
Thibaut <jobardthibaut12@gmail.com>
2023-12-11 20:30:10 +0100

    Refactor game logic and improve performance

--- d4/d04_2.kt
+++ d4/d04_2.kt
@@ -1,75 +1,49 @@
 import java.io.File
-import kotlin.math.pow
 
 fun main() {
-    val filePath = "/Users/b513im/Documents/advent_of_code/d4/d4.txt"
+    val filePath = "d4.txt"
     val maps = mutableListOf<String>()
 
     File(filePath).useLines { lines ->
         maps.addAll(lines)
     }
-    var jeuEvoluer = JeuEvoluer()
-    jeuEvoluer.maps.addAll(maps)
-    tourPartie(jeuEvoluer)
-}
+    val result = part2(input = maps)
+    println(result)
 
-fun tourPartie(jeuEvoluer: JeuEvoluer) {
-    if(jeuEvoluer.round == jeuEvoluer.cartesBonus.size) {
-        jeuEvoluer.cartesBonus.add(0)
-    }
-    if(jeuEvoluer.maps.size > jeuEvoluer.round) {
-        val updatedJeuEvoluer = createList(jeuEvoluer.maps[jeuEvoluer.round], jeuEvoluer)
-        val resultat = updatedJeuEvoluer.cartesJouer.count{ it in updatedJeuEvoluer.cartesBanque}
-        if( resultat > 0) {
-            updatedJeuEvoluer.ancienPoints += 2.0.pow(resultat - 1).toInt()
-            updatedJeuEvoluer.points += 
-                2.0.pow(resultat - 1).toInt() * (updatedJeuEvoluer.cartesBonus[updatedJeuEvoluer.round]+1)
-        
-            for(i in updatedJeuEvoluer.round+1..updatedJeuEvoluer.round+1+resultat) {
-                if(i >= updatedJeuEvoluer.cartesBonus.size) {
-                    updatedJeuEvoluer.cartesBonus.add(1)
-                } else {
-                    updatedJeuEvoluer.cartesBonus[i] += updatedJeuEvoluer.cartesBonus[updatedJeuEvoluer.round]+1
+}
+ fun part2(input: List<String>): Int {
+        var ret = 0
+        var cards = ArrayList<Int>()
+        for(i in 0 until input.size) {
+            cards.add(1)
+        }
+        for(i in 0 until input.size) {
+            var s = input[i]
+            var l = s.split("\\s+".toRegex())
+            var winners = HashSet<Int>()
+            var iswinner = true
+            var amt = 0
+            for(sidx in 2 until l.size) {
+                if(l[sidx] == "|") {
+                    iswinner = false
+                }
+                else {
+                    if(iswinner) {
+                        winners.add(l[sidx].toInt())
+                    }
+                    else {
+                        if(winners.contains(l[sidx].toInt())) {
+                            amt++
+                        }
+                    }
                 }
             }
+            for(j in 0 until amt) {
+                cards[i+j+1] += cards[i]
+            }
         }
-        updatedJeuEvoluer.totalCartesAGratte += updatedJeuEvoluer.cartesBonus[updatedJeuEvoluer.round]+1
-        updatedJeuEvoluer.round += 1
-        tourPartie(updatedJeuEvoluer)  
-    } else {
-        println("Fin de la partie resultat: ${jeuEvoluer.points} carte a gratter: ${jeuEvoluer.totalCartesAGratte} : et ancien points: ${jeuEvoluer.ancienPoints}")
-    }
-}
-
-fun createList(round: String, jeuEvoluer: JeuEvoluer): JeuEvoluer{
-    val mapSplit = round.split(":")
-    val mapGame = mapSplit[1].trim().split("|")
-    val joueur = mapGame[0].trim().split(" ")
-    val banque = mapGame[1].trim().split(" ")
-    
-    val joueurList = mutableListOf<Int>()
-    val banqueList = mutableListOf<Int>()
-    for(nombre in joueur) {
-        if(nombre != "") {
-            joueurList.add(nombre.toInt())
+        for(x in cards) {
+            ret += x
         }
+        return ret
     }
-    for(nombre in banque) {
-        if(nombre != "") {
-            banqueList.add(nombre.toInt())
-        }
-    }
-    return jeuEvoluer.copy(cartesJouer = joueurList, cartesBanque = banqueList)
-}
-data class JeuEvoluer(
-    var points: Int = 0,
-    var ancienPoints: Int = 0,
-    var round: Int = 0, 
-    var cartesBonus: MutableList<Int> = mutableListOf(0,0,0,0,0,0),
-    val maps: MutableList<String> = mutableListOf(),
-
-    var cartesJouer: MutableList<Int> = mutableListOf(),
-    var cartesBanque: MutableList<Int> = mutableListOf(),
-
-    var totalCartesAGratte: Int = 0,
-)
